a5caa5714cadf14199c07a8226a82cbbde66fd17
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-04-16 18:03:19 +0300

    Add an import from class user and repo

--- src/app/Components/profile/profile.component.ts
+++ src/app/Components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Users } from 'src/app/classes/users';
+import { Repos } from 'src/app/classes/repos';
+import { SearchRequestService } from '../search-http/search-request.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user!: Users;
+  repos:any;
+
+  constructor(public searchService: SearchRequestService) {
+
+   }
+
+   searchUser(username:string){
+    this.searchService.getUserProfile(username).then((success)=>{
+     this.user = this.searchService.user;
+    },
+    (error)=>{
+      console.log(error)
+    });
+    this.searchService.getUserRepo(username).then((success)=>{
+     this.repos = this.searchService.repos;
+    },
+    (error)=>{
+      console.log(error)
+    });
+  }
 
   ngOnInit(): void {
+    this.searchUser("SharonneKemboi");
   }
 
 }
+
