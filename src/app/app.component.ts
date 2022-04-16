23416dd5edc594d419882c0473a4cf28906edc0d
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-04-17 01:19:13 +0300

    Fix merge conflict

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
   title = 'Github-Search';
-=======
-  title = 'GitSearchApp';
->>>>>>> ae1dd57ec6dae27633b92117a83565b93effa088
 }
