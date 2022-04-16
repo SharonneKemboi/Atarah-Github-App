412b3f3b79520863afc17e26b7969e2d89b4193c
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-04-16 18:33:24 +0300

    create a property apiUrl and assigned it to Github API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  clientSecret: 'your client secret',
+  clientId: 'your client id'
+
 };
 
 /*
