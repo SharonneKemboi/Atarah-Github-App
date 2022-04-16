01586b1ce0b2dcfff25416a86500bbfadf1d62f9
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-04-16 17:57:36 +0300

    Generate Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { RepositoriesComponent } from './Components/repositories/repositories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProfileComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
