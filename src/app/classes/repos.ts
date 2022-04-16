a3cdc6d05d44f0f504a9a51716ac2dee2c39b331
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-04-16 17:54:54 +0300

    Declare data types

--- src/app/classes/repos.ts
+++ src/app/classes/repos.ts
@@ -1,2 +1,9 @@
 export class Repos {
+
+    constructor(
+        public name: string,
+        public html_url:string,
+        public description:string,
+        public language:string, 
+        public created_at:Date){}
 }
